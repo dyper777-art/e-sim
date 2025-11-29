b161e4c41ae2037f942245d7a8ba150cf53be4bf
ME <me@192.168.1.200>
2025-11-30 02:40:40 +0700

    commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use KHQR\BakongKHQR;
 
 class TestController extends Controller
 {
     public function test(Request $request)
     {
+        $bakongKHQR = new BakongKHQR('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiNDE2ZTY1NWQ0ZGM3NGUwMiJ9LCJpYXQiOjE3NjQ0MDg1MTEsImV4cCI6MTc3MjE4NDUxMX0.DSCa77FRMGSPenx0t6uiyBVPaxSp0Ms7yF4Dgt53Uro');
+
         return response()->json([
-            'message' => 'hello'
+            'message' => $bakongKHQR
         ], 200);
     }
 }
