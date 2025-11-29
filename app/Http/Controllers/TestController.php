390b7cda20afbe62f3ee21e75002ce2b113a61c3
ME <me@192.168.1.200>
2025-11-30 03:05:35 +0700

    commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -28,10 +28,6 @@ class TestController extends Controller
 
     public function otherTest(Request $request){
         $bakongKHQR = new BakongKHQR('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiNDE2ZTY1NWQ0ZGM3NGUwMiJ9LCJpYXQiOjE3NjQ0MDg1MTEsImV4cCI6MTc3MjE4NDUxMX0.DSCa77FRMGSPenx0t6uiyBVPaxSp0Ms7yF4Dgt53Uro');
-        $response = $bakongKHQR->checkTransactionByMD5('21a85838d75b501b0706d709e236652c');
-
-        return response()->json([
-            'message' => $response
-        ], 200);
+        $bakongKHQR->checkTransactionByMD5('21a85838d75b501b0706d709e236652c');
     }
 }
